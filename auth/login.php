e64bc0e93b45a5b502c5f1b77a0cd0f16e7b5fd0
muriha094 <muhamadrifkyh09@gmail.com>
2025-01-29 22:48:29 +0700

    merevisi code & tambah password visibility toggle

--- auth/login.php
+++ auth/login.php
@@ -1,3 +1,12 @@
+<?php 
+
+require_once "../config.php";
+
+$sekolah = mysqli_query($koneksi, "SELECT * FROM tbl_sekolah WHERE id = 1");
+$data = mysqli_fetch_array($sekolah);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,41 +15,60 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Login - SB Admin</title>
-        <link href="css/styles.css" rel="stylesheet" />
+        <title>Login - SMK MURIHA</title>
+        <link href="<?= $main_url ?>asset/sb-admin/css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+        <link rel="icon" type="image/x-icon" href="<?= $main_url ?>asset/image/school.png">
+        <style>
+            #bgLogin {
+                background-image: url("../asset/image/<?= $data['gambar'] ?>");
+                background-size: cover;
+                background-position: center center;
+            }
+            .password-wrapper {
+                position: relative;
+            }
+            .password-wrapper button {
+                position: absolute;
+                top: 50%;
+                right: 10px;
+                transform: translateY(-50%);
+                border: none;
+                background: none;
+                cursor: pointer;
+            }
+        </style>
     </head>
-    <body class="bg-primary">
+    <body id="bgLogin">
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <main>
-                    <div class="container">
+                    <div class="container mt-4">
                         <div class="row justify-content-center">
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <div class="card-header">
+                                        <h4 class="text-center font-weight-light my-4">Login - SMK MURIHA</h4>
+                                    </div>
                                     <div class="card-body">
-                                        <form>
+                                        <form action="proses-login.php" method="POST">
                                             <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                                                <label for="inputEmail">Email address</label>
+                                                <input class="form-control" id="username" name="username" type="text" pattern="[A-Za-z0-9]{3,}" title="kombinasi huruf minimal 3 karakter" placeholder="Username" autocomplete="off" required />
+                                                <label for="username">Username</label>
                                             </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" />
+                                            <div class="form-floating mb-3 password-wrapper">
+                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" minlength="4" name="password" required />
                                                 <label for="inputPassword">Password</label>
+                                                <button type="button" id="toggle-password">
+                                                    <i class="bi bi-eye" id="icon-password"></i>
+                                                </button>
                                             </div>
-                                            <div class="form-check mb-3">
-                                                <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
-                                                <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
-                                            </div>
-                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <a class="small" href="password.html">Forgot Password?</a>
-                                                <a class="btn btn-primary" href="index.html">Login</a>
-                                            </div>
+                                            <button type="submit" name="login" class="btn btn-primary col-12 rounded-pill my-2">Login</button>
                                         </form>
                                     </div>
                                     <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+                                        <div class="text-muted small">Copyright &copy; SMK MURIHA <?= date("Y") ?></div>
                                     </div>
                                 </div>
                             </div>
@@ -48,22 +76,24 @@
                     </div>
                 </main>
             </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
         </div>
+        <script>
+            document.getElementById("toggle-password").addEventListener("click", function() {
+                const passwordField = document.getElementById("inputPassword");
+                const iconPassword = document.getElementById("icon-password");
+
+                if (passwordField.type === "password") {
+                    passwordField.type = "text";
+                    iconPassword.classList.remove("bi-eye");
+                    iconPassword.classList.add("bi-eye-slash");
+                } else {
+                    passwordField.type = "password";
+                    iconPassword.classList.remove("bi-eye-slash");
+                    iconPassword.classList.add("bi-eye");
+                }
+            });
+        </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
+        <script src="<?= $main_url ?>asset/sb-admin/js/scripts.js"></script>
     </body>
 </html>
